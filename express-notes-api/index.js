d3a2266542a87fec224a9acc4838f148779e1357
Gaven Loughridge <gavenlou@gmail.com>
2021-08-16 23:43:52 -0700

    User can replace a note using PUT.

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -20,7 +20,7 @@ app.get('/api/notes/:id/', (req, res) => {
   for (let key in data.notes) {reply.push(key);}
   if (req.params.id < 0 || isNaN(req.params.id)) {
     res.status(400)
-    return res.json({"error": "id must be a positive integer"});
+    return res.json({"error": "id must be a positive integer."});
   }
   if (!reply.includes(req.params.id)) {
     res.status(404);
@@ -33,7 +33,7 @@ app.get('/api/notes/:id/', (req, res) => {
 app.post('/api/notes', (req, res) => {
   if (!req.body.content) {
     res.status(400);
-    return res.json({ "error": "content is a required field"})
+    return res.json({ "error": "content is a required field."})
   }
   res.status(201);
   data.notes[data.nextId] = req.body;
@@ -53,11 +53,11 @@ app.delete('/api/notes/:id', (req, res) => {
   for (let key in data.notes) { reply.push(key); }
   if (req.params.id < 0 || isNaN(req.params.id)) {
     res.status(400);
-    return res.json({ "error": "id must be a positive integer"})
+    return res.json({ "error": "id must be a positive integer."});
   }
   if (!reply.includes(req.params.id)) {
     res.status(404);
-    return res.json({ "error": "cannot find note with id " + req.params.id})
+    return res.json({ "error": "cannot find note with id " + req.params.id});
   }
   delete data.notes[req.params.id];
   fs.writeFile('data.json', JSON.stringify(data, null, 2), (err) => {
@@ -70,7 +70,34 @@ app.delete('/api/notes/:id', (req, res) => {
       return res.send()
     }
   })
+})
 
+app.put('/api/notes/:id', (req, res) => {
+  let reply = [];
+  for (let key in data.notes) { reply.push(key); }
+  if (req.params.id < 0 || isNaN(req.params.id)) {
+    res.status(400);
+    return res.json({ "error": "id must be a positive integer." })
+  }
+  if (!req.body.content) {
+    res.status(400);
+    return res.json({ "error": "content is a required field." })
+  }
+  if (!reply.includes(req.params.id)) {
+    res.status(404);
+    return res.json({ "error": "cannot find note with id " + req.params.id });
+  }
+  data.notes[req.params.id].content = req.body.content;
+  fs.writeFile('data.json', JSON.stringify(data, null, 2), (err) => {
+    if (err) {
+      res.status(500);
+      return res.json({ "error": "An unexpected error occurred." });
+    }
+    else {
+      res.status(200);
+      return res.json(data.notes[req.params.id]);
+    }
+  })
 })
 
 app.listen(3000, () => {
